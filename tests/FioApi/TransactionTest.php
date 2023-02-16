9de6a6ce6122875d5b7470bd003abda2e6f4fb97
Vláďa Urbánek <urbanek.vlada@gmail.com>
2023-02-16 21:12:25 +0000

    Upload New File

---
+++ tests/FioApi/TransactionTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace FioApi;
+
+class TransactionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAccountValuesAreProperlySet()
+    {
+        $transaction = json_decode(file_get_contents(__DIR__.'/data/example-transaction.json'));
+        $transaction = Transaction::createFromJson($transaction);
+
+        $this->assertSame(1111111111, $transaction->getId());
+        $this->assertEquals(new \DateTime('2015-03-30+0200'), $transaction->getDate());
+        $this->assertSame(127.0, $transaction->getAmount());
+        $this->assertSame('CZK', $transaction->getCurrency());
+        $this->assertSame('214498596', $transaction->getAccountNumber());
+        $this->assertSame('2100', $transaction->getBankCode());
+        $this->assertSame('0', $transaction->getVariableSymbol());
+        $this->assertSame(null, $transaction->getConstantSymbol());
+        $this->assertSame(null, $transaction->getSpecificSymbol());
+        $this->assertSame('Banka, a.s.', $transaction->getBankName());
+        $this->assertSame('HUJER MARTIN', $transaction->getUserIdentity());
+        $this->assertSame('Platba eshop', $transaction->getUserMessage());
+        $this->assertSame('Bezhotovostní příjem', $transaction->getTransactionType());
+        $this->assertSame(null, $transaction->getPerformedBy());
+        $this->assertSame('Comment? Comment!', $transaction->getComment());
+        $this->assertSame(1111122222, $transaction->getPaymentOrderId());
+        $this->assertSame('1500.00 EUR', $transaction->getSpecification());
+    }
+
+    public function testRemittanceInfoWithShortMessage()
+    {
+        $remittanceInfo = 'Rent for apartment door 12';
+        $tx = Transaction::create((object) [
+            'date'        => new \DateTime('2016-05-01'),
+            'amount'      => '66.00',
+            'currency'    => 'USD',
+            'userMessage' => $remittanceInfo,
+        ]);
+
+        $this->assertEquals($remittanceInfo, $tx->getRemittanceInfo1());
+        $this->assertEquals('', $tx->getRemittanceInfo2());
+        $this->assertEquals('', $tx->getRemittanceInfo3());
+        $this->assertEquals('', $tx->getRemittanceInfo4());
+    }
+
+    public function testRemittanceInfoWith140CharacterMessage()
+    {
+        $remittanceInfo = [
+            'Lorem ipsum dolor sit amet, consect',
+            'etur adipiscing elit. Nulla arcu ur',
+            'na, condimentum in justo eu, sceler',
+            'isque vulputate nisl. Sed volutpat.',
+        ];
+
+        $tx = Transaction::create((object) [
+            'date'        => new \DateTime('2016-07-20'),
+            'amount'      => 120.00,
+            'currency'    => 'CZK',
+            'userMessage' => implode($remittanceInfo),
+        ]);
+
+        $this->assertEquals($remittanceInfo[0], $tx->getRemittanceInfo1());
+        $this->assertEquals($remittanceInfo[1], $tx->getRemittanceInfo2());
+        $this->assertEquals($remittanceInfo[2], $tx->getRemittanceInfo3());
+        $this->assertEquals($remittanceInfo[3], $tx->getRemittanceInfo4());
+    }
+
+    public function testBenefFields()
+    {
+        $tx = Transaction::create((object) [
+            'date'         => new \DateTime(),
+            'amount'       => 200.00,
+            'currency'     => 'CAD',
+            'benefName'    => 'Petr Kramar',
+            'benefStreet'  => 'Andelova 12',
+            'benefCity'    => 'Ostrava',
+            'benefCountry' => 'CZ',
+        ]);
+
+        $this->assertEquals('Petr Kramar', $tx->getBenefName());
+        $this->assertEquals('Andelova 12', $tx->getBenefStreet());
+        $this->assertEquals('Ostrava', $tx->getBenefCity());
+        $this->assertEquals('CZ', $tx->getBenefCountry());
+    }
+}
